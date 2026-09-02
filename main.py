d07e9de2811286312bb75e65ef761e14263394a5
JepsXR <juanes501lol@gmail.com>
2026-09-02 18:08:21 -0500

    Enhance UserData model with additional fields
    Added fields for age, learning style, and available hours to UserData model.

--- main.py
+++ main.py
@@ -30,4 +30,6 @@ else:
 class UserData(BaseModel):
 
     name: str = Field(min_length=3, max_length=20)
-    
+    age: int = Field(...,gt=10, lt=100)
+    learning_style: Literal["Visual", "Auditory", "Kinesthetic", "Reading/Writing"] = Field(...,)
+    available_hours_per_day: float = Field(gt=0, lt=24)

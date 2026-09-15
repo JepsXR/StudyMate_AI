469e725590a6690cbf2c3f1517c956f1621bae93
JepsXR <juanes501lol@gmail.com>
2026-09-15 09:26:54 -0500

    Strengthen database security with mapped_column and enum

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import List, Optional, Literal
 from dotenv import load_dotenv
-from sqlalchemy import Column, Integer, String, Boolean, Float, create_engine
+from sqlalchemy import Column, Integer, String, Float, create_engine, Enum
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
 import json
 import os
@@ -72,11 +72,13 @@ session = SessionLocal()
 class User(base):
     __tablename__ = "Users"
     id = Column(Integer, primary_key=True)
-    name: Mapped[str]
-    age: Mapped[int]
-    strongest_learning_style: Mapped[str]
-    info_learning_style: Mapped[str]
-    available_hours_per_day: Mapped[float]
+    name: Mapped[str] = mapped_column(String(20))
+    age: Mapped[int] = mapped_column(Integer(100))
+    strongest_learning_style: Mapped[Literal[
+        "Visual", "Auditory", "Kinesthetic", "Reading/Writing", "Other"]] = mapped_column(
+            Enum("Visual", "Auditory", "Kinesthetic", "Reading/Writing", "Other"), default="Other")
+    info_learning_style: Mapped[str] = mapped_column(String(100))
+    available_hours_per_day: Mapped[float] = mapped_column(Float(24))
     study_subjects: Mapped[list[str]]
     concentration_level: Mapped[str]
     info_exam: Mapped[str]
